70f623526d979ace4e53f806decda6f920461026
nklenke <nick_klenke@yahoo.com>
2020-05-18 11:47:48 -0500

    initial commit

--- projects/capstone/starter/app.py
+++ projects/capstone/starter/app.py
@@ -2,11 +2,212 @@ import os
 from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+import json
+from models import setup_db, Movie, Actor, db, db_drop_and_create_all
+from auth import AuthError, requires_auth
 
 def create_app(test_config=None):
-  # create and configure the app
   app = Flask(__name__)
+  setup_db(app)
   CORS(app)
+  db_drop_and_create_all()
+
+  @app.after_request
+  def after_request(response):
+      response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization, true')
+      response.headers.add('Access-Control-Allow-Methods', 'GET, PATCH, POST, DELETE, OPTIONS')
+      response.headers.add('Access-Control-Allow-Origin', '*')
+      return response
+
+  @app.route('/actors')
+  @requires_auth('get:actors')
+  def get_actors(payload):
+      actors = Actor.query.all()
+      actors = [actor.format() for actor in actors]
+      return jsonify(actors)
+
+  @app.route('/actors', methods=['POST'])
+  @requires_auth('post:actors')
+  def create_actor(payload):
+      req = request.get_json()
+      name = req['name']
+      gender = req['gender']
+      role = req['role']
+
+      if name is None:
+          abort(400)
+      try:
+          actor = Actor(name=name, gender=gender, role=role)
+          actor.insert()
+          new_actor = actor.to_dict()
+
+          return jsonify({
+              'success': True,
+              'new_actor': new_actor
+          }), 200
+      except Exception:
+          abort(422)
+
+  @app.route('/actors/<int:id>', methods=['PATCH'])
+  @requires_auth('patch:actors')
+  def update_actor(payload, id):
+      actor = Actor.query.filter(Actor.id == id).one_or_none()
+
+      if actor is None:
+          abort(404)
+
+      req = request.get_json()
+      if req is None:
+          abort(400)
+
+      name = req.get('name')
+      gender = req.get('gender')
+      role = req.get('role')
+
+      try:
+          if name:
+              actor.name = name
+
+          if gender:
+              actor.gender = gender
+
+          if role:
+              actor.gender = role
+
+          actor.update()
+
+          updated_actor = [actor.to_dict()]
+
+          return jsonify({
+              'success': True,
+              'actor': updated_actor
+          }), 200
+
+      except Exception:
+          abort(422)
+
+  @app.route('/actors/<int:id>', methods=['DELETE'])
+  @requires_auth('delete:actors')
+  def delete_actor(payload, id):
+      actor = Actor.query.filter(Actor.id == id).one_or_none()
+
+      if actor is None:
+          abort(404)
+
+      actor.delete()
+
+      return jsonify({
+          'success': True,
+          'delete': id,
+      }), 200
+
+  @app.route('/movies')
+  @requires_auth('get:movies')
+  def get_movies(payload):
+      movies = Movie.query.all()
+      movies = [movie.format() for movie in movies]
+      return jsonify(movies)
+
+  @app.route('/movies', methods=['POST'])
+  @requires_auth('post:movies')
+  def create_movie(payload):
+      req = request.get_json()
+      title = req.get('title')
+      release_date = req.get('release_date', None)
+
+      if title is None:
+          abort(400)
+      try:
+          movie = Movie(title=title, release_date=release_date)
+          movie.insert()
+          new_movie = movie.to_dict()
+
+          return jsonify({
+              'success': True,
+              'new_movie': new_movie
+          }), 200
+      except Exception:
+          abort(422)
+
+  @app.route('/movies/<int:id>', methods=['PATCH'])
+  @requires_auth('patch:movies')
+  def update_movie(payload, id):
+      movie = Movie.query.filter(Movie.id == id).one_or_none()
+
+      if movie is None:
+          abort(404)
+
+      req = request.get_json()
+      if req is None:
+          abort(400)
+
+      title = req.get('title')
+      release_date = req.get('release_date')
+
+      try:
+          if title:
+              movie.title = title
+
+          if release_date:
+              movie.release_date = release_date
+
+          movie.update()
+
+          updated_movie = [movie.to_dict()]
+
+          return jsonify({
+              'success': True,
+              'movie': updated_movie
+          }), 200
+
+      except Exception:
+          abort(422)
+
+  @app.route('/movies/<int:id>', methods=['DELETE'])
+  @requires_auth('delete:movies')
+  def delete_movie(payload, id):
+      movie = Movie.query.filter(Movie.id == id).one_or_none()
+
+      if movie is None:
+          abort(404)
+
+      movie.delete()
+
+      return jsonify({
+          'success': True,
+          'delete': id
+      }), 200
+
+  # Error Handling
+  @app.errorhandler(400)
+  def bad_request(error):
+      return jsonify({
+          "success": False,
+          "error": 400,
+          "message": "bad request"
+      }), 400
+
+  @app.errorhandler(404)
+  def not_found(error):
+      return jsonify({
+          "success": False,
+          "error": 404,
+          "message": "resource not found"
+      }), 404
+
+  @app.errorhandler(422)
+  def unprocessable(error):
+      return jsonify({
+          "success": False,
+          "error": 422,
+          "message": "unprocessable"
+      }), 422
+
+  @app.errorhandler(AuthError)
+  def auth_error(error):
+      return jsonify({
+          "message": error.error
+      }), error.status_code
 
   return app
 
--- projects/capstone/starter/test_app.py
+++ projects/capstone/starter/test_app.py
@@ -1,1 +1,215 @@
 
+import os
+import unittest
+import json
+from flask_sqlalchemy import SQLAlchemy
+import datetime
+from app import create_app
+from models import setup_db, db_drop_and_create_all, Actor, Movie
+
+
+class CastingTestCase(unittest.TestCase):
+    def setUp(self):
+        """Define test variables and initialize app."""
+        self.app = create_app()
+        self.client = self.app.test_client
+
+        self.casting_assistant = os.getenv('CASTING_ASSISTANT')
+        self.casting_director = os.getenv('CASTING_DIRECTOR')
+        self.executive_producer = os.getenv('EXECUTIVE_PRODUCER')
+
+        self.database_filename = "database.db"
+        self.project_dir = os.path.dirname(os.path.abspath(__file__))
+        self.database_path = "sqlite:///{}".format(os.path.join(self.project_dir, self.database_filename))
+
+        #self.database_name = "casting_test"
+        #self.database_path = "postgres://{}:{}@{}/{}".format(
+        #    'postgres', 'willa2018', 'localhost:5432', self.database_name)
+        setup_db(self.app, self.database_path)
+        db_drop_and_create_all()
+
+        self.new_actor = {
+            'name': 'Nick Klenke',
+            'gender': 'Male',
+            'role': 'Programmer'
+        }
+
+        self.new_movie = {
+            'title': 'New Movie',
+            'release_date' : datetime.date(2020, 5, 18),
+        }
+
+        # binds the app to the current context
+        with self.app.app_context():
+            self.db = SQLAlchemy()
+            self.db.init_app(self.app)
+            # create all tables
+            db_drop_and_create_all()
+
+    def tearDown(self):
+        pass
+
+    def test_get_actors(self):
+        res = self.client().get('/actors')
+        self.assertEqual(res.status_code, 200)
+    
+    def test_fetch_all_actors_casting_assistant_first(self):
+        res = self.client().get('/actors',
+                                headers={
+                                    "Authorization": "Bearer {}".format(
+                                        self.casting_assistant)
+                                })
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'resource not found')
+
+
+    '''
+    def test_get_all_categories(self):
+        response = self.client().get('/categories')
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(len(data['categories']))
+
+    def test_get_paginated_questions(self):
+        response = self.client().get('/questions')
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(len(data['questions']))
+        self.assertTrue(data['total_questions'])
+        self.assertTrue(len(data['categories']))
+
+    def test_404_get_paginated_questions_invalid_page(self):
+        response = self.client().get('/questions?page=1000')
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Resource Not Found')
+
+    def test_delete_question(self):
+        question = Question(
+            question='test question',
+            answer='test answer',
+            difficulty=1,
+            category=1)
+        question.insert()
+        question_id = question.id
+
+        response = self.client().delete(f'/questions/{question_id}')
+        data = json.loads(response.data)
+
+        question = Question.query.filter(
+            Question.id == question.id).one_or_none()
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(data['deleted'], question_id)
+        self.assertEqual(question, None)
+
+    def test_404_delete_question_invalid_id(self):
+        response = self.client().delete('/questions/zzzzz')
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Resource Not Found')
+
+    def test_post_question_new(self):
+        new_question = {
+            'question': 'test question',
+            'answer': 'test answer',
+            'difficulty': 1,
+            'category': 1
+        }
+
+        total_questions_before = len(Question.query.all())
+        response = self.client().post('/questions', json=new_question)
+        data = json.loads(response.data)
+        total_questions_after = len(Question.query.all())
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data["success"], True)
+        self.assertEqual(total_questions_after, total_questions_before + 1)
+
+    def test_422_post_question_new_missing_data(self):
+        new_question = {
+            'question': 'test question',
+            'answer': 'test answer',
+            'category': 1
+        }
+        response = self.client().post('/questions', json=new_question)
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 422)
+        self.assertEqual(data["success"], False)
+        self.assertEqual(data["message"], "Unprocessable")
+
+    def test_post_question_search(self):
+        search_data = {'searchTerm': 'a'}
+        response = self.client().post('/questions', json=search_data)
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertIsNotNone(data['questions'])
+        self.assertIsNotNone(data['total_questions'])
+
+    def test_404_post_question_search(self):
+        search_data = {'searchTerm': 'zzzzz'}
+        response = self.client().post('/questions', json=search_data)
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(data["success"], False)
+        self.assertEqual(data["message"], "Resource Not Found")
+
+    def test_get_questions_per_category(self):
+        response = self.client().get('/categories/1/questions')
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(len(data['questions']))
+        self.assertTrue(data['total_questions'])
+        self.assertTrue(data['current_category'])
+
+    def test_404_get_questions_per_category(self):
+        response = self.client().get('/categories/1000/questions')
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(data["success"], False)
+        self.assertEqual(data["message"], "Resource Not Found")
+
+    def test_get_quiz_questions(self):
+        request_data = {
+            'previous_questions': [],
+            'quiz_category': {
+                'type': 'Entertainment',
+                'id': 5}}
+        response = self.client().post('/quizzes', json=request_data)
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['question'])
+
+    def test_400_get_quiz_questions(self):
+        request_data = {'previous_questions': []}
+        response = self.client().post('/quizzes', json=request_data)
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(data["success"], False)
+        self.assertEqual(data["message"], "Bad Request")
+    '''
+
+# Make the tests conveniently executable
+if __name__ == "__main__":
+    unittest.main()
